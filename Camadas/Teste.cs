7fc97e7bf96e54145e5ac456fc53a14d31baa363
Carlos Alberto Minafra  Junior <minafra@oi.com.br>
2012-10-16 21:16:48 -0300

    criada uma classe teste
    classe criada para

---
+++ Camadas/Teste.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Camadas
+{
+    class Teste
+    {
+        //esta e uma classe teste
+    }
+}
